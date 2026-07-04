0659e2c956a7d7b10b1eb18df3567295d2b48726
Anth0o0ny <pushkin.a.s.03@mail.ru>
2026-07-04 07:39:59 +0300

    chore(project): initialize kotlin spring multi-service workspace

---
+++ dispute-workflow-service/src/main/kotlin/com/payflow/disputes/workflow/DisputeWorkflowServiceApplication.kt
@@ -0,0 +1,11 @@
+package com.payflow.disputes.workflow
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DisputeWorkflowServiceApplication
+
+fun main(args: Array<String>) {
+    runApplication<DisputeWorkflowServiceApplication>(*args)
+}
