72f1a38ba09e94573fa0c611fac548aba9a883e4
Anth0o0ny <pushkin.a.s.03@mail.ru>
2026-07-21 23:25:38 +0300

    fix(transaction): return ok for screening endpoint

--- transaction-service/src/main/kotlin/com/payflow/disputes/transaction/api/controller/TransactionController.kt
+++ transaction-service/src/main/kotlin/com/payflow/disputes/transaction/api/controller/TransactionController.kt
@@ -3,12 +3,10 @@ package com.payflow.disputes.transaction.api.controller
 import com.payflow.disputes.transaction.api.dto.CreateTransactionRequest
 import com.payflow.disputes.transaction.api.dto.TransactionResponse
 import com.payflow.disputes.transaction.service.TransactionService
-import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.server.ResponseStatusException
 import java.util.UUID
@@ -18,7 +16,6 @@ class TransactionController(
     private val transactionService: TransactionService
 ) {
     @PostMapping("/api/transactions")
-    @ResponseStatus(HttpStatus.CREATED)
     fun create(@RequestBody request: CreateTransactionRequest): TransactionResponse =
         TransactionResponse.from(transactionService.create(request))
 
